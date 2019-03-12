f56af93e07afe2f061650badf382ace6401eb776
IrisSaskia <irisoostra@upcmail.nl>
2019-03-12 21:51:02 +0100

    Auto vroem vroem

---
+++ Frogger/GameObjects/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Frogger {
+	class Car : GameObject {
+		public Car(String assetName, Vector2 position, Vector2 velocity) : base(assetName) {
+			this.position = position;
+			this.velocity = velocity;
+		}
+
+		public override void Init() {
+			base.Init();
+			
+		}
+
+		public override void Update() {
+			base.Update();
+			
+			position += velocity;
+			if(position.X > GameEnvironment.Screen.X) {
+				position.X = -1 * texture.Width - 10;
+			}
+		}
+	}
+}
--- Frogger/GameStates/PlayingState.cs
+++ Frogger/GameStates/PlayingState.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
-namespace Frogger
-{
-    class PlayingState : GameState
-    {
-        public PlayingState()
-        {
+namespace Frogger {
+    class PlayingState : GameState {
+        public PlayingState() {
             gameObjectList.Add(new GameObject("spr_background"));
             gameObjectList.Add(new Frog());
+			gameObjectList.Add(new Car("spr_dozer", new Vector2(3, 150), new Vector2(1, 0)));
         }
     }
 }
